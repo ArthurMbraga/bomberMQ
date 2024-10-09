c1675257c214420a083eb7e27f8e4393c12caad5
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-10-09 20:58:34 +0200

    Improving code organization

---
+++ src/components/index.ts
@@ -0,0 +1,153 @@
+import {
+  AreaComp,
+  Comp,
+  GameObj,
+  ScaleComp,
+  SpriteComp,
+  TileComp,
+  TimerComp,
+  Vec2,
+} from "kaplay";
+
+export function destructible(): Comp {
+  return {
+    id: "destructible",
+    add() {
+      const spriteComp = this as SpriteComp;
+      const tileComp = this as TileComp;
+      spriteComp.onAnimEnd((anim) => {
+        if (anim === "explode") {
+          // destroy(this as any);
+          tileComp.getLevel().remove(this as any);
+        }
+      });
+    },
+  };
+}
+
+export function bomb(): Comp {
+  return {
+    id: "bombComp",
+    require: ["scale", "pos", "timer", "area"],
+    add() {
+      const areaComp = this as AreaComp;
+      areaComp.onCollideEnd("player", () => {
+        //this.
+      });
+
+      const timerComp = this as TimerComp;
+      timerComp.wait(2, () => {
+        const tileComp = this as TileComp;
+        const level = tileComp.getLevel();
+        level.spawn("*", tileComp.tilePos);
+        destroy(this as any);
+      });
+    },
+    update() {
+      const scaleComp = this as ScaleComp;
+      const variation = 0.15;
+      scaleComp.scaleTo(1 + -1 * variation * 0.5 * (Math.sin(time() * 8) + 1));
+    },
+  };
+}
+
+type OnCreateComp = {
+  isFirstTime: boolean;
+  onCreate?: Function;
+} & Comp;
+
+export function withOnCreate(): OnCreateComp {
+  return {
+    id: "withOnCreate",
+    isFirstTime: true,
+    update() {
+      if (this.isFirstTime && this.onCreate) {
+        this.onCreate();
+        this.isFirstTime = false;
+      }
+    },
+  };
+}
+
+type ExplodeComp = {
+  direction: Vec2;
+  force: number;
+};
+
+export function explode(): Comp & Partial<OnCreateComp> & Partial<ExplodeComp> {
+  return {
+    id: "explode",
+    require: ["pos", "withOnCreate", "rotate"],
+    add() {},
+    onCreate() {
+      const tileComp = this as TileComp & ExplodeComp;
+      const level = tileComp.getLevel();
+
+      function containsWall(objs: GameObj[]) {
+        return objs.some((o) => o.is("wall"));
+      }
+      function canDetonateAt(pos: Vec2) {
+        const objs = level.getAt(pos);
+        return !containsWall(objs);
+      }
+      function detonateAt(pos: Vec2) {
+        const objs = level.getAt(pos);
+        let detonated = false;
+
+        objs.map((obj) => {
+          if (obj.c("destructible")) {
+            const spriteComp = obj.c("sprite") as SpriteComp;
+            spriteComp.play("explode");
+
+            detonated = true;
+          }
+        });
+
+        return detonated;
+      }
+
+      function spawnExplosion(pos: Vec2, dir: Vec2) {
+        const obj = level.spawn("*", pos);
+
+        if (!obj) return;
+        obj.direction = dir;
+        obj.force = tileComp.force - 1;
+
+        // Select sprite
+        if (obj.force === 1) obj.play("point");
+        else obj.play("horizontal");
+
+        // Rotate the explosion
+        if (dir === UP) obj.rotateTo(-90);
+        else if (dir === DOWN) obj.rotateTo(-270);
+        else if (dir === LEFT) obj.rotateTo(180);
+        else if (dir === RIGHT) obj.rotateTo(0);
+      }
+
+      // 1. case -> propagate to all directions
+      if (!this.direction) {
+        const directions = [UP, DOWN, LEFT, RIGHT];
+        for (const dir of directions) {
+          const pos = tileComp.tilePos;
+          const nextPos = pos.add(dir);
+
+          if (canDetonateAt(nextPos) && !detonateAt(nextPos))
+            spawnExplosion(nextPos, dir);
+        }
+      }
+      // 2. case -> propagate to a specific direction
+      else if (tileComp.force > 1) {
+        const pos = tileComp.tilePos;
+        const nextPos = pos.add(this.direction);
+
+        if (canDetonateAt(nextPos) && !detonateAt(nextPos))
+          spawnExplosion(nextPos, this.direction);
+      }
+    },
+    update() {
+      // const pos = this as TileComp;
+      // console.log("Explode", pos.tilePos);
+      // const pos = this as PosComp;
+    },
+  };
+}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,6 @@
-import {
-  Comp,
-  GameObj,
-  Key,
-  ScaleComp,
-  SpriteComp,
-  TileComp,
-  TimerComp,
-  Vec2,
-} from "kaplay";
+import { Key, Vec2 } from "kaplay";
+import { bomb, destructible, explode, withOnCreate } from "./components";
 
-// load assets
 loadSprite("bean", "/sprites/bean.png");
 loadSprite("bomberman_front", "/sprites/bomberman_front.png");
 loadSprite("coin", "/sprites/coin.png");
@@ -37,155 +28,6 @@ loadSprite("fire", "/sprites/fire.png", {
 
 const SPEED = 320;
 const TILE_SIZE = 64;
-
-type BlinkComp = Comp;
-
-function blink(): BlinkComp {
-  return {
-    id: "blink",
-    require: ["scale", "pos", "timer"],
-    add() {
-      const timerComp = this as TimerComp;
-      timerComp.wait(2, () => {
-        const tileComp = this as TileComp;
-        const level = tileComp.getLevel();
-        level.spawn("*", tileComp.tilePos);
-        destroy(this as any);
-      });
-    },
-    update() {
-      const scaleComp = this as ScaleComp;
-      // Oscila entre 0.5 e 1
-      const variation = 0.15;
-      const posComp = this as TileComp;
-      scaleComp.scaleTo(1 + -1 * variation * 0.5 * (Math.sin(time() * 8) + 1));
-    },
-  };
-}
-
-type Test = {
-  isFirstTime: boolean;
-  onCreate?: Function;
-  setOnCreate(func: () => void): void;
-};
-
-type OnCreateComp = Comp & Test;
-
-function withOnCreate(): OnCreateComp {
-  return {
-    id: "withOnCreate",
-    isFirstTime: true,
-    setOnCreate(func) {
-      this.onCreate = func;
-    },
-    update() {
-      if (this.isFirstTime && this.onCreate) {
-        this.onCreate();
-        this.isFirstTime = false;
-      }
-    },
-  };
-}
-
-function destructable(): Comp {
-  return {
-    id: "destructable",
-    add() {
-      const spriteComp = this as SpriteComp;
-      const tileComp = this as TileComp;
-      spriteComp.onAnimEnd((anim) => {
-        if (anim === "explode") {
-          // destroy(this as any);
-          tileComp.getLevel().remove(this as any);
-        }
-      });
-    },
-  };
-}
-
-type ExplodeComp = {
-  direction: Vec2;
-  force: number;
-};
-
-function explode(): Comp & Partial<OnCreateComp> & Partial<ExplodeComp> {
-  return {
-    id: "explode",
-    require: ["pos", "withOnCreate", "rotate"],
-    add() {},
-    onCreate() {
-      const tileComp = this as TileComp & ExplodeComp;
-      const level = tileComp.getLevel();
-
-      function isWall(objs: GameObj[]) {
-        return objs.some((o) => o.is("wall"));
-      }
-
-      function isDestroyable(objs: GameObj[]) {
-        return objs.some((o) => {
-          if (o.is("wood")) console.log("wood", o);
-          return o.is("wood");
-        });
-      }
-
-      function detonateAt(pos: Vec2) {
-        const objs = level.getAt(pos);
-        console.log("detonateAt", pos, objs);
-        if (isWall(objs)) return false;
-
-        if (isDestroyable(objs)) {
-          objs.map((obj) => {
-            const spriteComp = obj.c("sprite") as SpriteComp;
-            if (obj.c("destructable")) spriteComp.play("explode");
-          });
-          return false;
-        }
-        return true;
-      }
-
-      function spawnExplosion(pos: Vec2, dir: Vec2) {
-        const obj = level.spawn("*", pos);
-
-        if (!obj) return;
-        obj.direction = dir;
-        obj.force = tileComp.force - 1;
-
-        // Select sprite
-        if (obj.force === 1) obj.play("point");
-        else obj.play("horizontal");
-
-        // Rotate the explosion
-        if (dir === UP) obj.rotateTo(-90);
-        else if (dir === DOWN) obj.rotateTo(-270);
-        else if (dir === LEFT) obj.rotateTo(180);
-        else if (dir === RIGHT) obj.rotateTo(0);
-      }
-
-      // 1. case -> propagate to all directions
-      if (!this.direction) {
-        const directions = [UP, DOWN, LEFT, RIGHT];
-        for (const dir of directions) {
-          const pos = tileComp.tilePos;
-          const nextPos = pos.add(dir);
-
-          if (detonateAt(nextPos)) spawnExplosion(nextPos, dir);
-        }
-      }
-      // 2. case -> propagate to a specific direction
-      else if (tileComp.force > 1) {
-        const pos = tileComp.tilePos;
-        const nextPos = pos.add(this.direction);
-
-        if (detonateAt(nextPos)) spawnExplosion(nextPos, this.direction);
-      }
-    },
-    update() {
-      // const pos = this as TileComp;
-      // console.log("Explode", pos.tilePos);
-      // const pos = this as PosComp;
-    },
-  };
-}
 
 scene("game", () => {
   const level = addLevel(
@@ -217,7 +59,7 @@ scene("game", () => {
           anchor("center"),
           scale(0.5),
           timer(),
-          blink(),
+          bomb(),
           "bomb",
           { playerId: 3 },
         ],
@@ -239,10 +81,9 @@ scene("game", () => {
         "+": () => [
           sprite("wood", { width: TILE_SIZE, height: TILE_SIZE }),
           area(),
-          destructable(),
+          destructible(),
           body({ isStatic: true }),
           anchor("center"),
-          "wood",
         ],
         "*": () => [
           sprite("fire", { width: TILE_SIZE, height: TILE_SIZE }),
@@ -267,8 +108,6 @@ scene("game", () => {
     area(),
     body(),
     anchor("center"),
-    // "player",
-    // { playerId: 1 },
   ]);
 
   const dirs = {
