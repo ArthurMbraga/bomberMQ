ecb84ff25e5820371abadafc35f8338482e16542
Arthur Martins Braga <arthur.martinsbraga@telecom-paris.fr>
2024-10-09 21:02:29 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,139 +1,30 @@
-import { Key, Vec2 } from "kaplay";
-import { bomb, destructible, explode, withOnCreate } from "./components";
+import { Boot } from './scenes/Boot';
+import { Game as MainGame } from './scenes/Game';
+import { GameOver } from './scenes/GameOver';
+import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
 
-loadSprite("bean", "/sprites/bean.png");
-loadSprite("bomberman_front", "/sprites/bomberman_front.png");
-loadSprite("coin", "/sprites/coin.png");
-loadSprite("spike", "/sprites/spike.png");
-loadSprite("wall", "/sprites/wall.png");
-loadSprite("wood", "/sprites/wood-explode.png", {
-  sliceX: 5,
-  sliceY: 2,
-  anims: {
-    explode: { from: 0, to: 9, speed: 30 },
-    idle: 0,
-  },
-});
-loadSprite("ghosty", "/sprites/ghosty.png");
-loadSprite("bomb", "/sprites/16bit_bomb1.png");
-loadSprite("fire", "/sprites/fire.png", {
-  sliceX: 3,
-  sliceY: 1,
-  anims: {
-    center: 0,
-    horizontal: 1,
-    point: 2,
-  },
-});
+import { Game, Types } from "phaser";
 
-const SPEED = 320;
-const TILE_SIZE = 64;
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 1024,
+    height: 768,
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        MainGame,
+        GameOver
+    ]
+};
 
-scene("game", () => {
-  const level = addLevel(
-    [
-      "========================",
-      "=@      ++      $$     =",
-      "= = = = = = = = = = = ==",
-      "=      ++       ++     =",
-      "= = = = = = = = = = = ==",
-      "=^      ++      ##     =",
-      "========================",
-    ],
-    {
-      tileWidth: TILE_SIZE,
-      tileHeight: TILE_SIZE,
-      pos: vec2(TILE_SIZE * 2, TILE_SIZE * 2),
-      tiles: {
-        $: () => [
-          sprite("bomberman_front", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          body(),
-          anchor("center"),
-          "player",
-          { playerId: 2 },
-        ],
-        "0": () => [
-          sprite("bomb", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          anchor("center"),
-          scale(0.5),
-          timer(),
-          bomb(),
-          "bomb",
-          { playerId: 3 },
-        ],
-        "#": () => [
-          sprite("bomberman_front", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          body(),
-          anchor("center"),
-          "player",
-          { playerId: 4 },
-        ],
-        "=": () => [
-          sprite("wall", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          body({ isStatic: true }),
-          anchor("center"),
-          "wall",
-        ],
-        "+": () => [
-          sprite("wood", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          destructible(),
-          body({ isStatic: true }),
-          anchor("center"),
-        ],
-        "*": () => [
-          sprite("fire", { width: TILE_SIZE, height: TILE_SIZE }),
-          area(),
-          withOnCreate(),
-          anchor("center"),
-          pos(),
-          rotate(0),
-          explode(),
-          opacity(1),
-          lifespan(0.5, { fade: 0.5 }),
-          { direction: undefined, force: 4 },
-          "explosion",
-        ],
-      },
-    }
-  );
-
-  const player = add([
-    sprite("bomberman_front", { width: TILE_SIZE, height: TILE_SIZE }),
-    pos(TILE_SIZE * 3, TILE_SIZE * 3),
-    area(),
-    body(),
-    anchor("center"),
-  ]);
-
-  const dirs = {
-    left: LEFT,
-    right: RIGHT,
-    up: UP,
-    down: DOWN,
-  };
-
-  for (const dir in dirs) {
-    onKeyDown(dir as Key, () => {
-      player.move(dirs[dir as keyof typeof dirs].scale(SPEED));
-    });
-  }
-
-  onKeyPress("space", () => {
-    level.spawn("0", posToTile(player.pos.sub(TILE_SIZE * 2, TILE_SIZE * 2)));
-  });
-
-  player.onCollide("explosion", () => {
-    destroy(player);
-  });
-});
-
-function posToTile(pos: Vec2) {
-  return vec2(Math.round(pos.x / TILE_SIZE), Math.round(pos.y / TILE_SIZE));
-}
-
-go("game");
+export default new Game(config);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,1 @@
 /// <reference types="vite/client" />
-
-import 'kaplay/global'
-
-declare module '*.png' {
-  const src: string
-  export default src
-}
